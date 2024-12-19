b73b41b2b72cd289fcf03f1e7e684efe9804cd71
jonhrafe <jonhrafe@gmail.com>
2024-12-19 03:07:26 +0100

    "Makefile and compile.sh updated. Benchmark option included."

--- compile.sh
+++ compile.sh
@@ -7,11 +7,11 @@ cd src
 COMMON_FILES="simulablesequence.cpp vertex.cpp obstacle.cpp collision.cpp scheme.cpp voxel.cpp cylinder.cpp walker.cpp \
 mcsimulation.cpp parallelmcsimulation.cpp trajectory.cpp triangle.cpp parameters.cpp plyobstacle.cpp pgsesequence.cpp \
 dynamicsSimulation.cpp simerrno.cpp collisionsphere.cpp cylindergammadistribution.cpp sentinel.cpp subdivision.cpp \
-gradientwaveform.cpp propagator.cpp sphere.cpp sphere.h spheregammadistribution.cpp spheregammadistribution.h"
+gradientwaveform.cpp propagator.cpp sphere.cpp sphere.h spheregammadistribution.cpp benchmark.cpp"
 
 # Build MC-DC_Simulator
 echo "Compiling MC-DC_Simulator..."
-g++ -O3 -std=c++11 -lpthread -std=c++0x -pthread -w -I. main.cpp $COMMON_FILES -o ../MC-DC_Simulator
+g++ -O3 -std=c++17 -march=native -flto -lpthread -pthread -w -I. main.cpp $COMMON_FILES -o ../MC-DC_Simulator
 if [ $? -ne 0 ]; then
     echo "Error compiling MC-DC_Simulator"
     exit 1
@@ -19,7 +19,7 @@ fi
 
 # Build dataSynth
 echo "Compiling dataSynth..."
-g++ -O3 -std=c++11 -lpthread -std=c++0x -pthread -w -I. dataSynth.cpp $COMMON_FILES -o ../dataSynth
+g++ -O3 -std=c++17 -march=native -flto -lpthread -pthread -w -I. dataSynth.cpp $COMMON_FILES -o ../dataSynth
 if [ $? -ne 0 ]; then
     echo "Error compiling dataSynth"
     exit 1
